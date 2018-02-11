f253e81d893d8594fd8d5516a9dd549a6c4ed3b9
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-02-11 14:58:24 -0500

    Dynamic components - form example

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,34 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <app-footer v-bind:title="title"></app-footer>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show form one</button>
+    <button v-on:click="component = 'form-two'">Show form two</button>
   </div>
 </template>
 
 
 <script>
 
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue';
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
 
 export default {
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninjas': Ninjas
+    'form-one': formOne,
+    'form-two': formTwo
   },
 
   data () {
     return {
-      ninjas: [
-        {name: 'Ryu', speciality: 'Vue Components', show: false},
-        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-        {name: 'Hitoshi', speciality: 'Click Events', show: false},
-        {name: 'Tango', speciality: 'Conditionals', show: false},
-        {name: 'Kami', speciality: 'Webpack', show: false},
-        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-      ],
-      title: 'Vue Ninjas'
+      component: 'form-one'
     }
   },
 
   methods: {
-    updateTitle: function(updatedTitle) {
-      this.title = updatedTitle
+
     }
-  }
 }
 </script>
 
