89da75109b205cc1d89c973e399a617111c86293
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-02-11 22:12:35 -0500

    Add routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <show-blogs></show-blogs>
-    <list-blogs></list-blogs>
+    <app-header></app-header>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -11,12 +11,14 @@
 import addBlog from './components/addBlog.vue'
 import showBlogs from './components/showBlogs.vue'
 import listBlogs from './components/listBlogs.vue'
+import header from './components/header.vue'
 
 export default {
   components: {
     'add-blog': addBlog,
     'show-blogs': showBlogs,
-    'list-blogs': listBlogs
+    'list-blogs': listBlogs,
+    'app-header': header
   },
 
   data () {
